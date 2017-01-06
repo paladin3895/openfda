5f96709f5de06575b337afc146fba612ee65b764
dat <dat@coachingcloud.com>
2017-01-06 22:34:25 +0700

    add verbose to fetch file

--- src/fetchFile.js
+++ src/fetchFile.js
@@ -6,5 +6,6 @@ const dataDir = path.join(__dirname, '../data');
 const resources = JSON.parse(fs.readFileSync(path.join(__dirname, '../resources.json')));
 
 resources.forEach((url) => {
+  console.log(`...Fetching ${url}...`);
   execSync(`./bin/getResource.sh ${url} ${dataDir}`);
 });
