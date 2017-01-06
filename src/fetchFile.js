b4004d89478599612cf45569143f6a787cff22f8
dat <dat@coachingcloud.com>
2017-01-06 22:13:53 +0700

    modify resources for other data

--- src/fetchFile.js
+++ src/fetchFile.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { execSync } from 'child_process';
 
 const dataDir = path.join(__dirname, '../data');
-const resources = JSON.parse(fs.readFileSync(path.join(__dirname, '../resources.mockup.json')));
+const resources = JSON.parse(fs.readFileSync(path.join(__dirname, '../resources.json')));
 
 resources.forEach((url) => {
   execSync(`./bin/getResource.sh ${url} ${dataDir}`);
