8cd52285b2dccac988e28f5a3f07828782fe5650
dat <dat@coachingcloud.com>
2017-01-06 17:21:23 +0700

    add chunk file into smaller pieces

---
+++ src/chunkFile.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import uuid from 'uuid';
+import _ from 'lodash';
+
+const dataDir = path.join(__dirname, '../data');
+const chunkDir = path.join(dataDir, 'chunks');
+const resources = JSON.parse(fs.readFileSync(path.join(__dirname, '../resources.mockup.json')));
+const limit = 1000;
+
+if (!fs.existsSync(chunkDir)) {
+  fs.mkdirSync(chunkDir);
+}
+
+resources.forEach((url) => {
+  const filename = url.split('/').pop().replace('.zip', '');
+  const data = JSON.parse(fs.readFileSync(path.join(dataDir, filename)));
+  const length = data.results.length;
+
+  _.range(0, length, limit)
+   .forEach(() => {
+     fs.writeFileSync(
+       path.join(chunkDir, uuid.v1()),
+       JSON.stringify(data.results.splice(0, limit)));
+   });
+});
