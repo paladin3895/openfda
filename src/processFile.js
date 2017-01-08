c2b7f0a10e0d8914b856e6a80f986f02f31482f2
dat.pham <duydatyds@gmail.com>
2017-01-08 10:43:00 +0700

    modify process file

--- src/processFile.js
+++ src/processFile.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
+import zlib from 'zlib';
 
 import { Report, Patient, Drug, Reaction } from './models';
 import { mapDataToModels } from './helper';
@@ -19,17 +20,23 @@ console.log(`Processing filename: ${filename}`);
 
 fs.writeFileSync(path.join(chunkDir, 'index'), filenames.join(os.EOL));
 
-const data = JSON.parse(fs.readFileSync(path.join(chunkDir, filename)));
+const data = JSON.parse(
+  zlib.unzipSync(fs.readFileSync(filename)));
 
 data.forEach((report) => {
-  Report.create(
-    mapDataToModels(report),
-    {
-      include: [
-        { model: Patient, as: 'patient' },
-        { model: Drug, as: 'drugs' },
-        { model: Reaction, as: 'reactions' },
-      ]
-    }
-  ).catch(({ name, message }) => fs.appendFile(logFile, `${name}: ${message}${os.EOL}`));
+  Report
+    .findOne({ where: { safetyReportId: report.safetyreportid } })
+    .then((result) => {
+      if (result) { return; }
+      return Report.create(
+        mapDataToModels(report),
+        {
+          include: [
+            { model: Patient, as: 'patient' },
+            { model: Drug, as: 'drugs' },
+            { model: Reaction, as: 'reactions' },
+          ]
+        })
+    })
+    .catch(({ name, message }) => fs.appendFile(logFile, `${name}: ${message}${os.EOL}`));
 });
