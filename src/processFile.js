69446d4a2f234d6ae8ac3555fd750af04a90a109
dat <dat@coachingcloud.com>
2017-01-19 11:49:45 +0700

    implement api to get data

--- src/processFile.js
+++ src/processFile.js
@@ -2,41 +2,40 @@ import fs from 'fs';
 import path from 'path';
 import os from 'os';
 import zlib from 'zlib';
+import AWS from 'aws-sdk';
+// import Promise from 'bluebird';
 
-import { Report, Patient, Drug, Reaction } from './models';
-import { mapDataToModels } from './helper';
+// import { Report, Patient, Drug, Reaction, Substance } from './models';
+// import { mapDataToModels } from './helper';
 
 const logFile = path.join(__dirname, '../.log');
-const chunkDir = path.join(__dirname, '..', 'data', 'chunks');
 
-const filenames = fs.readFileSync(path.join(chunkDir, 'index')).toString().split(os.EOL);
+require('dotenv').config();
 
-console.log(`Total file: ${filenames.length}`);
+const config = new AWS.Config({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION,
+  httpOptions: { timeout: 300000 },
+});
 
-const filename = filenames.shift();
-if (!filename) { process.exit(); }
+const s3 = new AWS.S3(config);
+const filename = process.argv[2];
 
-console.log(`Processing filename: ${filename}`);
+s3.getObject({
+  Bucket: 'foxience',
+  Key: `openfda.adr/${filename}`,
+}, (err, data) => {
+    // Handle any error and exit
+  if (err) {
+    const { message } = err;
+    fs.appendFile(logFile, `Unable to get data: ${message}${os.EOL}`);
+    return;
+  }
 
-fs.writeFileSync(path.join(chunkDir, 'index'), filenames.join(os.EOL));
+  const reports = JSON.parse(zlib.unzipSync(data.Body).toString());
 
-const data = JSON.parse(
-  zlib.unzipSync(fs.readFileSync(filename)));
+  reports.forEach((report) => {
 
-data.forEach((report) => {
-  Report
-    .findOne({ where: { safetyReportId: report.safetyreportid } })
-    .then((result) => {
-      if (result) { return; }
-      return Report.create(
-        mapDataToModels(report),
-        {
-          include: [
-            { model: Patient, as: 'patient' },
-            { model: Drug, as: 'drugs' },
-            { model: Reaction, as: 'reactions' },
-          ]
-        });
-    })
-    .catch(({ name, message }) => fs.appendFile(logFile, `${name}: ${message}${os.EOL}`));
+  });
 });
---
+++ src/server.js
@@ -0,0 +1,49 @@
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import zlib from 'zlib';
+import restify from 'restify';
+import { acceptParser, queryParser, bodyParser } from 'restify-plugins';
+
+const server = restify.createServer({
+  name: 'Automation API',
+  version: '1.0.0'
+});
+
+server.use(restify.CORS({
+  origins: ['*'],
+  credentials: true,                 // defaults to false
+  methods: ['GET', 'PUT', 'DELETE', 'POST', 'OPTIONS']
+}));
+
+server.use(acceptParser(server.acceptable));
+server.use(queryParser());
+server.use(bodyParser());
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  return next();
+});
+
+const dataDir = path.join(__dirname, '..', 'data', 'adr');
+
+server.get('/openfda/adr', (req, res, next) => {
+  const index = fs.readFileSync(path.join(dataDir, 'index')).toString().trim().split(os.EOL);
+  res.send({
+    status: 1,
+    data: index,
+  });
+  next();
+});
+
+server.get('/openfda/adr/:id', (req, res, next) => {
+  const block = fs.readFileSync(path.join(dataDir, req.params.id));
+  res.send({
+    status: 1,
+    data: JSON.parse(zlib.unzipSync(block)),
+  });
+  next();
+});
+
+server.listen(8383, () => {
+  console.log('%s listening at %s', server.name, server.url);
+});
